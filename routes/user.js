92ccdddc21521c7988682e06925961411fb3c547
lzh <linzhaohan@fjzzhy.cn>
2023-06-01 17:12:06 +0800

    注册功能

--- routes/user.js
+++ routes/user.js
@@ -10,25 +10,28 @@ var secretOrPrivateKey = "vueTestAdmin"
 async function getJWTTokenData(token) {
     return jwt.verify(token, secretOrPrivateKey, async (err, decode) => {
         if (err) { //  时间失效的时候/ 伪造的token
-            console.log(err,'err');
             return "";
         } else {
-            console.log(decode,'decode');
             return decode;
         }
     });
 }
+//获取登录用户个人信息
 router.get('/getUser', async function (req, res) {
-    console.log(req.headers['x-token'], 'req');
     var token = req.headers['x-token']
     var msg=await getJWTTokenData(token)
-    console.log(msg);
     // var params = req.query;
-    var id=msg.id
+    var id = msg.id
+    if (!id) {
+        res.json({
+            code: -1,
+            msg:'获取身份信息失败'
+        })
+        return
+    }
     var sql = "select * from re_user where id=?"
     args = [id]
     db.queryOne(sql, args).then(ret => {
-        console.log(ret, 'ret');
         res.json({
             stuUser: ret,
             code: 200,
@@ -37,26 +40,6 @@ router.get('/getUser', async function (req, res) {
     })
 })
 
-// router.post('/getUserInfo', async function (req,res) {
-//     var params = req.body;
-//     var username=params.username
-//     var password=params.password
-//     var sql = "select * from re_user where username=? and password=?"
-//     args = [username,password]
-//     db.queryOne(sql, args).then(ret => {
-//         if (!ret) {
-//             res.json({
-//                 code: -1,
-//                 msg:'用户名不存在或密码错误'
-//             })
-//             return
-//         }
-//         console.log(ret, 'ret');
-//         res.json({
-//             data: ret
-//         })
-//     })
-// })
 
 router.post('/login', async function (req,res) {
     var params = req.body;
@@ -84,4 +67,30 @@ router.post('/login', async function (req,res) {
         })
     })
 })
+
+router.post('/register', async function (req,res) {
+    var params = req.body;
+    var username=params.username
+    var password=params.password
+    var sql = "select * from re_user where username=?"
+    args = [username]
+    db.queryOne(sql, args).then(async ret => {
+        if (ret) {
+            res.json({
+                code: -1,
+                msg:'用户名已存在'
+            })
+            return
+        }
+        var newUser=new DBObj()
+        newUser.set('id', UUID.v1().replace(/-/g, ""))
+        newUser.set('username',username)
+        newUser.set('password',password)
+        await db.execute("insert into re_user set ?",[newUser.format()])
+        res.json({
+            msg: '操作成功'
+        })
+    })
+})
+
 module.exports = router
